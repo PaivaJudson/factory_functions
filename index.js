5050ac254e4173eb2c58880b7b8a6804c3f1cd8b
judsonpaiva <judson.paiva@hotmail.com>
2022-08-09 18:54:09 +0100

    function factory - parte II

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-function criaPessoa(nome, sobreNome) {
+function criaPessoa(nome, sobreNome, altura, peso) {
     return {
         nome,
-        sobreNome
+        sobreNome,
+
+        fala: function(assunto) {
+            return `${this.nome} está falando ${assunto}`
+        },
+        altura: altura,
+        peso: peso,
+        get imc() {
+            const i = this.peso / (this.altura ** 2);
+            return i.toFixed(2);
+        },
+
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobreNome = valor.join(' ');
+        },
+
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobreNome}`;
+        }
     };
 }
 
-const p1 = criaPessoa('Judson', 'Otavio');
-console.log(p1);
+
+const p1 = criaPessoa('Judson', 'Paiva', 1.8, 80);
+p1.nomeCompleto = "Leonor Barbas Sebastião José Paiva";
+console.log(p1.fala('sobre JS'), '\ncálculo do IMC = ' + p1.imc);
+console.log('Nome Completo: ' + p1.nomeCompleto);
